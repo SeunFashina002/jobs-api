23f9caea5d9cb07e248422e6c662f6b2b2a35893
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-12 13:33:41 -0700

    created models job and user model

---
+++ models/Jobs.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const jobSchema = mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Please provide a company name"],
+    maxlength: 50,
+  },
+  position: {
+    type: String,
+    required: [true, "Please provide a position"],
+    maxlength: 100,
+  },
+  status: {
+    type: String,
+    enum: ["interview", "declined", "pending"],
+    default: "pending",
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "This field cannot be left empty"],
+    ref: "user",
+  },
+});
+
+module.exports = mongoose.model("Job", jobSchema);
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please provide a name"],
+      maxlenght: 50,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: [true, "please provide an email"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "please provide a password"],
+      minlength: 6,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
