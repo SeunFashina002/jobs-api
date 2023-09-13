bf1bfb03eb46d21d13882d4f244aacfc3f8f761c
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-13 10:10:38 -0700

    updated routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { register, login } = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post("/", register);
-router.post("/", login);
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
