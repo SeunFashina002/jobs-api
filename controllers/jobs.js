0db7b687a3cb453f04863a29faaa7462c07f200d
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-18 13:46:14 -0700

    added route handler for jobs routes'

---
+++ controllers/jobs.js
@@ -0,0 +1,16 @@
+const Jobs = require("../models/Jobs");
+const { StatusCodes } = require("http-status-codes");
+
+const createJob = async (req, res) => {
+  try {
+    req.body.createdBy = req.user.id;
+    const job = await Jobs.create(req.body);
+    res.status(StatusCodes.CREATED).json({ success: true, data: job });
+  } catch (err) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, error: err.message });
+  }
+};
+
+// TODO: Get all jobs
