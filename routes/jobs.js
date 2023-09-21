aaf09b16114a2eedf4dccfe93b718dd444db8202
Seun Fashina <92753117+SeunFashina002@users.noreply.github.com>
2023-09-21 21:39:29 +0100

    Update jobs.js

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,7 +3,7 @@ const { createJob, getJobs} = require("../controllers/jobs");
 
 const router = express.Router();
 
-//Todo
+
 router.post("/", createJob);
 router.get("/", getJobs)
 router.get("/:id", getJob)
