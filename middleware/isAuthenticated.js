9cb904798e0481208136014841f1d2d3777da225
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-27 17:31:04 -0700

    fix error response bug

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -7,17 +7,24 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const isAuthenticated = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw Error("Oops, you need to login");
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ success: false, error: "Oops, you need to login" });
   }
 
   const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, JWT_SECRET);
     // attach it to the job request headers
-    req.user = { id: payload.id, name: payload.name };
+    if (!req.user) {
+      req.user = { id: payload.id, name: payload.name };
+    }
+
     next();
   } catch (err) {
-    throw Error("Oops, you need to login");
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "Oops, you need to login" });
   }
 };
 
